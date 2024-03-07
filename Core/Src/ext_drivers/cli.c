6e7cd24c7d34b093c7f1c05e193fa7f0d02ad9f5
cole <cab572@drexel.edu>
2024-03-06 22:47:59 -0500

    removed unnecessary brackets in tokenize func

--- Core/Src/ext_drivers/cli.c
+++ Core/Src/ext_drivers/cli.c
@@ -52,14 +52,8 @@ int tokenize(char *s, char *toks[], int maxtoks, char *delim)
 	toks[i] = (char *)strtok(s, delim);
 	while(toks[i++] != NULL)
 	{
-		if(i >= maxtoks -1)
-		{
-			toks[i] = NULL;
-		}
-		else
-		{
-			toks[i] = (char *)strtok(NULL, delim);
-		}
+		if(i >= maxtoks -1) toks[i] = NULL;
+		else toks[i] = (char *)strtok(NULL, delim);
 	}
 	return i;
 }
